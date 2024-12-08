960b071105177f384f229dabd2c1aa10333b776a
MarcusMendesR <marcusmendes746@gmail.com>
2024-12-08 13:22:31 -0300

    continuação do curso de js

---
+++ particularidades-js/js/scripts.js
@@ -0,0 +1,10 @@
+console.log("Primeiro");
+
+console.log("Ultimo");
+
+//tipagem fraca//
+var a = 1;
+
+a = "teste";
+
+console.log(a);
---
+++ variaveis-js/js/scripts.js
@@ -0,0 +1,24 @@
+//criando variaveis//
+
+var teste = 1;
+console.log(teste);
+
+
+//é possivel alterar a tipagem mas pode ser perigoso//
+teste = "Marcus";
+
+console.log(teste);
+
+var $nome = 2;
+console.log($nome);
+
+var _nome = 4;
+console.log(_nome);
+
+//Outras formas de declarar variaveis//
+
+let testando = 5;
+const ola = 2;
+
+console.log(testando);
+console.logo(ola);

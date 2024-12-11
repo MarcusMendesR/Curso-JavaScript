3d84a32d6982fe5acf238a377884ed7b733b6e13
MarcusMendesR <marcusmendes746@gmail.com>
2024-12-11 09:37:51 -0300

    Tipos de dados

---
+++ arrays/js/scripts.js
@@ -0,0 +1,20 @@
+var arr = [5, "Marcus", true];
+// O array aceita qualquer tipo de dados;
+console.log(arr);
+
+var arr2 = [5,3,6,1,2];
+console.log(arr2);
+
+//O array começa em 0 e é possivel acessar determinado elemento dentro dele;
+
+console.log(arr[1]);
+//Acessa o segundo elemento;
+
+// inserindo elementos no array;
+
+ arr[4] = 10;
+// cria um index que não existe no array;
+
+// mudando o elemento;
+arr[0] = "teste";
+console.log(arr);
---
+++ objetos/js/script.js
@@ -0,0 +1,14 @@
+var obj = {
+    nome : "Marcus",
+    idade : "20",
+    profissao: "desempregado",
+};
+
+console.log(obj);
+console.log(typeof obj);
+
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.profissao);
+
+console.log("O meu nome e"+obj.nome);

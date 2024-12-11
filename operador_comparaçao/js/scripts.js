3527e7a2e56688a6d87b9576f0c596e90dfa9e06
MarcusMendesR <marcusmendes746@gmail.com>
2024-12-11 09:59:12 -0300

    Operadores de comparação

---
+++ operador_comparaçao/js/scripts.js
@@ -0,0 +1,24 @@
+var idade =18;
+var possuiCarro = 0;
+if (idade >= 18) {
+    console.log("O usuario pode tirar a carteira")    
+}
+
+if (idade <=17) {
+    console.log("O ususario não pode tirar a carteira");
+}
+
+if (possuiCarro) {
+    console.log("O usuario ja pode andar de carro");
+}
+// O js considera o 0 como false;
+
+var nome = "pedro";
+
+if (nome == "Marcus") {
+    console.log("O seu nome e Marcus");
+}
+
+if (nome != "Marcus") {
+    console.log("O nome nao e marcus");
+}

1afc7766a8203420fd89a736d1e569f40cb19222
MarcusMendesR <marcusmendes746@gmail.com>
2024-12-11 09:58:43 -0300

    Operador condicional

---
+++ if/js/scripts.js
@@ -0,0 +1,10 @@
+if(true){
+    console.log("Testando o if");
+}
+// se a instrução for verdadeira ele roda o if,se não ele pula o comando;
+
+var idade = 19;
+
+if(idade >18){
+    console.log("Ele pode tirar a carteira");
+}
